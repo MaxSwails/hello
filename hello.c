bcb465a60fc9cc7278cb3090b07f3e564f9d989a
MaxSwails <mswails@unomaha.edu>
2022-08-25 20:16:35 +0000

    initial command

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Max Swails
+ *         mswails@unomaha.edu
+ * Date: 2022/08/29
+ *
+ * A Hello Program that prints my name
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("My name is Max Swails\n");
+
+  return 0;
+}
